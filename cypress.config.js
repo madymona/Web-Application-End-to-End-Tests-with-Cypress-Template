3d9e05af5d603768376644d51d4d4cebb8a03b0d
madymona <91924296+madymona@users.noreply.github.com>
2024-08-30 11:43:21 -0400

    Create cypress.config.js

---
+++ cypress.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  e2e: {
+    baseUrl: 'http://localhost:3000', // Replace with your application's base URL
+    defaultCommandTimeout: 10000, // Set the default timeout for commands
+    supportFile: 'cypress/support/index.js', // Optional support file
+  },
+};
